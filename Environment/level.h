547e4a43701b550ce3462f3fa6a55bd735025f18
epiceliminator8@gmail.com <epiceliminator8@gmail.com>
2024-11-08 12:40:18 +0000

    Filling out 'level.h' file

--- Environment/level.h
+++ Environment/level.h
@@ -1,25 +1,56 @@
 #include <iostream>
+#include <vector>
 #include <SFML/Graphics.hpp>
+
+class Tile
+{
+private:
+	bool collision;
+	int tileType;
+
+public:
+
+	Tile(int type);
+	void Update();
+};
+
+class Room
+{
+private:
+	// Identification
+	int ID;
+
+	// Memory
+	static const int size = 16; // Preset size of rooms
+	Tile tileArray[size][size];
+
+	// Rendering tiles
+	sf::Sprite sprite;
+
+public:
+	int Size() { return size; }
+
+	Room(int arrayInput[size][size]);
+	void Update(float dt);
+	void Render(sf::Texture textures[15]);
+	
+	bool CollisionDetect();
+	
+};
 
 class Level
 {
 private:
-	//std::vector<Chunk> chunks;
+	std::vector<Room> rooms;
 
 public:
-	bool Init(int seed);
-	void Update();
+	Level();
+	bool Init();
+	void Generate(/*int seed*/);
+	void LoadRoom();
+	void Update(float dt);
 	void Render(sf::RenderWindow& window);
 
 };
 
-class Chunk
-{
-private:
-	
-};
-
-class Tile
-{
-
-};
+// Test Comment
